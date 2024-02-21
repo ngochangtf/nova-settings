29e1af08b0405e58124d785b7169aeb72d39cdf7
Han Nguyen <ngochan.gtf@gmail.com>
2024-02-21 15:59:49 +0700

    Add events before/after update settings

--- src/Traits/WithEvents.php
+++ src/Traits/WithEvents.php
@@ -3,32 +3,21 @@
 namespace Outl1ne\NovaSettings\Traits;
 
 use Closure;
-use Laravel\Nova\Fields\FieldCollection;
-use Laravel\Nova\Http\Requests\NovaRequest;
+use Illuminate\Http\Request;
 use Outl1ne\NovaSettings\NovaSettings;
 
 trait WithEvents
 {
-    protected function processWithEvents(
-        NovaRequest     $request,
-        FieldCollection $fields,
-        Closure         $callback,
-    ): mixed
+    protected function processWithEvents(Request $request, Closure $callback): mixed
     {
         if (is_callable(NovaSettings::$beforeUpdating)) {
-            call_user_func_array(NovaSettings::$beforeUpdating, [
-                'request' => &$request,
-                'fields' => &$fields,
-            ]);
+            call_user_func(NovaSettings::$beforeUpdating, $request);
         }
 
-        $result = $callback($request, $fields);
+        $result = $callback($request);
 
         if (is_callable(NovaSettings::$afterUpdated)) {
-            call_user_func_array(NovaSettings::$afterUpdated, [
-                'request' => &$request,
-                'result' => &$result,
-            ]);
+            call_user_func_array(NovaSettings::$afterUpdated, [$request, &$result]);
         }
 
         return $result;
